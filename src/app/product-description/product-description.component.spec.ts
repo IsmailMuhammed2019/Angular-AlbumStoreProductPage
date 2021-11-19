cee9822ad1447c53c2cd434e5706ca574ff02540
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2021-11-19 11:40:35 +0100

    corrected product description

--- src/app/product-description/product-description.component.spec.ts
+++ src/app/product-description/product-description.component.spec.ts
@@ -2,9 +2,6 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductDescriptionComponent } from './product-description.component';
 
-import { ProductService } from '../product.service';
-import { Component, OnInit } from '@angular/core';
-
 describe('ProductDescriptionComponent', () => {
   let component: ProductDescriptionComponent;
   let fixture: ComponentFixture<ProductDescriptionComponent>;
@@ -26,20 +23,3 @@ describe('ProductDescriptionComponent', () => {
     expect(component).toBeTruthy();
   });
 });
-
-@Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['.product-description.component.css']
-})
-
-export class ProductDescriptionComponent implements OnInit {
-
-  albumInfo;
-
-  constructor(private _productService: ProductService){ }
-
-  ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
-  }
-}
